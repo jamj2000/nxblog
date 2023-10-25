850900b891799d22997ee19ead80f99ad130f5e5
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2023-10-25 13:12:14 +0200

    ...

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import Image from "next/image"
 
 export const metadata = {
     title: "About Jose"
-  }
+}
 
 function About() {
-  return (
-    <section>
-      <h1>About</h1>
-      <hr />
-    </section>
-  )
+    return (
+        <section>
+            <h1>About</h1>
+            <hr />
+            <Image src="/avatar.png"
+                height={256}
+                width={256}
+                alt="Avatar of the author"
+                placeholder="blur"
+                blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
+                style={{marginRight: '20px', borderRadius: '50%', float: 'left'}}
+            />
+            <p>Hey, I am JAMJ200. I am vocational training proffesor of Computer Science in Spain. </p>
+            <p>This, what you see here, is just an experiment to develop my competence in coding the NextJS framework.
+So, basically, it is a fictitious blog.</p>
+            <p style={{ clear: 'both' }}></p>
+        </section>
+    )
 }
 
 export default About
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <section>
       <h1>Home Page</h1>
-      <hr />
+      <hr /><br />
+      <h3>Nothing here yet</h3>
+      <div>
+        <p>Try to visit the following links:</p>
+        <ul>
+          <li><Link href="/blog">Blog</Link></li>
+          <li><Link href="/about">About</Link></li>
+        </ul>
+      </div>
     </section>
   )
 }
